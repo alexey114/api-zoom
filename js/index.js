cf50ac32c0f804050f3a7cbdf5ff95e241f8a709
Bomko Alexey <bomko.alexey@cartweb.ru>
2022-08-30 14:10:10 +0300

    del error code

--- js/index.js
+++ js/index.js
@@ -1,62 +1,61 @@
 console.log('ok')
 
 function doGet(e){
-    //get authorisation code from returned URL
+    //получить код авторизации из возвращенного URL
     var authCode = JSON.stringify(e.parameter.code)
 
-    //remote quotes around the code
-    var authCodeClean = authCode.replace(/["']+/g, '')
+    //удаленные кавычки вокруг кода
+    var authCodeClean = authCode.replace(/['"]+/g, '')
 
-    //run function to add to Spreadsheet
-    saveIntoSheet(authCodeClean, 1)
+    //запустить функцию для добавления в электронную таблицу
+    // // saveIntoSheet(authCodeClean, 1)
 
 
-    //run Function to get Token from AuthCode
+    //запустить функцию, чтобы получить токен из AuthCode
     var accessToken = getToken(authCodeClean)
 
 
-    //check status of Access Token and display relevant message on webpage
+    //проверить статус токена доступа и отобразить соответствующее сообщение на веб-странице
     if(accessToken){
 
-        //return message to webpage to inform user
+        //вернуть сообщение на веб-страницу, чтобы проинформировать пользователя
         return HtmlService.createHtmlOutput('Success! You can close this tab.')
 
     } else {
         //there was a problem gettin Authentication Code
 
-        //return message to webpage to inform user
-        return HtmlService.createHtmlOutput('Failded. You can close this tab')
+        //вернуть сообщение на веб-страницу, чтобы проинформировать пользователя
+        return HtmlService.createHtmlOutput('Failed. You can close this tab')
     }
 }
 
-//use Authorisation Code to get a Token
+//используйте код авторизации для получения токена
 function getToken(authCodeClean) {
     
-    //set authentication and get OAuthKeys
+    //установить аутентификацию и получить OAuthKeys
 
     var clientID = 'N7fM9KQ0TRGiBbukGmfx3Q' //ENTER YOUR APP ID HERE
     var clientSecret = 'ofi3wvw8FBSp2UTefcffiUPHJpUd5X6l'  //ENTER YOUR APP SECRET HERE
-    var encodedKeys = Utikities.base64Encode(clientID + ":" + clientSecret)
+    var encodedKeys = Utilities.base64Encode(clientID + ":" + clientSecret)
 
-    //Set the HTTP headers
+    //Установите заголовки HTTP
     var options = {
         'method':"post",
         'headers':{"Authorization": "Basic" + encodedKeys}
     }
 
-
-    //Web App URL Linked to Zoom OAuth App
+    //URL-адрес веб-приложения, связанный с приложением Zoom OAuth
     var returnUrl = "https://bespoke-flan-8db314.netlify.app"
 
-    //make Zoom OAuth call
-    var response = UrlFetchApp.fetch("https://zoom.us/oauth/authorize?response_type=code&code" + authCodeClean + "&redirect_uri=" + returnUrl, options)
+    //сделать вызов Zoom OAuth
+    var response = UrlFetchApp.fetch("https://zoom.us/oauth/token?grant_type=authorization_code&code=" + authCodeClean + "&redirect_uri=" + returnUrl, options)
 
-    //run Function to add Access Token to Spreadsheet
-    var resultText = response.getContentText()
-    var resultObj = JSON.parse(resultText)
-    var accessToken = resultObj['access_token']
-    saveIntoSheet(accessToken, 2)
+    //запустить функцию, чтобы добавить токен доступа в электронную таблицу
+    // var resultText = response.getContentText()
+    // var resultObj = JSON.parse(resultText)
+    // var accessToken = resultObj['access_token']
+    // saveIntoSheet(accessToken, 2)
 
-    //return the Access Token value to the Parent Function
+    //вернуть значение токена доступа в родительскую функцию
     return accessToken;
 }
